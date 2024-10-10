e4d5a6f24411e1ca025e37790f7337efcecda794
Sergey Stepanenko <stepanenko@wamba.com>
2024-10-10 10:13:04 +0800

    update configuration

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   presets: [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript",
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
   ],
   plugins: [
-    process.env.NODE_ENV === "development" && "react-refresh/babel",
+    process.env.NODE_ENV === 'development' && 'react-refresh/babel',
   ].filter(Boolean),
-};
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,42 +1,44 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
-import webpack from "webpack";
-import "webpack-dev-server";
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import webpack from 'webpack'
+import 'webpack-dev-server'
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 const config: webpack.Configuration = {
-  mode: isDevelopment ? "development" : "production",
-  entry: "./src/index.tsx",
+  mode: isDevelopment ? 'development' : 'production',
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/", // For React Router
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/', // For React Router
   },
-  devtool: isDevelopment ? "inline-source-map" : false,
+  devtool: isDevelopment ? 'inline-source-map' : false,
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       // Add other loaders if necessary
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"],
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     isDevelopment && new ReactRefreshWebpackPlugin(),
   ].filter(Boolean) as webpack.WebpackPluginInstance[],
   devServer: {
     static: {
-      directory: path.join(__dirname, "public"),
+      // eslint-disable-next-line no-undef
+      directory: path.join(__dirname, 'public'),
     },
     hot: true,
     port: 3000,
@@ -45,7 +47,7 @@ const config: webpack.Configuration = {
       overlay: true,
     },
   },
-};
+}
 
 // Export the configuration
-export default config;
+export default config
