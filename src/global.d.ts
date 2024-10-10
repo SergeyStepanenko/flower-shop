7c9e89af9634d47ffde711822041afd2d5ef0fda
Sergey Stepanenko <stepanenko@wamba.com>
2024-10-10 11:01:27 +0800

    update configuration

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,8 @@
+import Telegram from '@types/telegram-web-app'
+
 /* eslint-disable no-unused-vars */
 interface Window {
   Telegram: {
-    WebApp: any // You can replace 'any' with the appropriate type if known
+    WebApp: Telegram // You can replace 'any' with the appropriate type if known
   }
 }
