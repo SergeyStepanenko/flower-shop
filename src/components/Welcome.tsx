c6ee1c4a6362471cfef1e7565cb6c82d7ac43f4b
Sergey Stepanenko <stepanenko@wamba.com>
2024-10-11 11:54:11 +0800

    update

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,19 +2,11 @@ import { styled } from '@linaria/react'
 import React, { FC } from 'react'
 
 export const Welcome: FC = () => {
-  const onClose = () => {
-    window.Telegram.WebApp.close()
-  }
-
-  return (
-    <div>
-      <Title>Добро пожаловать в мой Telegram Mini App!</Title>
-      <button onClick={onClose}>Close Mini App</button>
-    </div>
-  )
+  return <Title>Добро пожаловать в магазин цветов</Title>
 }
 
 const Title = styled.h1`
   font-size: 32px;
   color: #333;
+  text-align: center;
 `
